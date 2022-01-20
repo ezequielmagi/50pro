1d9b589dcc0f86f51539d3e9c9a9e0d6575bdafe
ezequiel magi <ezequielmaggi@gmail.com>
2022-01-20 16:11:07 -0300

    Sound Board finished

---
+++ 09 - Sound Board/main.js
@@ -0,0 +1,27 @@
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
+
+sounds.forEach( sound => {
+  const btn = document.createElement('button')
+  btn.classList.add('btn')
+
+  btn.innerText = sound
+  btn.addEventListener( 'click' , () => {
+  
+  stopSongs()
+
+  document.getElementById(sound).play()
+  })
+
+  const buttons = document.getElementById('buttons')
+  buttons.appendChild(btn)
+} )
+
+function stopSongs () { 
+  sounds.forEach( sound => {
+    const song = document.getElementById(sound)
+
+    song.pause()
+    song.currentTime = 0
+
+  })
+ }
